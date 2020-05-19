d9299513abc13326eda0d0932ac73d6b73df0bca
Boris Lebedev <global-mangal@yandex.ru>
2020-05-19 23:59:16 +0500

    Debug registation process

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -28,7 +28,7 @@ router.post(
 
         const candidate = await User.findOne({email})
         if (candidate) {
-            return res.statis(400).json({message: 'Такой пользователь уже существует... '})
+            return res.status(400).json({message: 'Такой пользователь уже существует... '})
         }
         const hashedPassword = await bcrypt.hash(password, 2)
         const user = new User({email, password: hashedPassword})
