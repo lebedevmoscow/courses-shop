8782bbac19cac58e9383b58914f87174396fcef8
Boris Lebedev <global-mangal@yandex.ru>
2020-05-19 21:58:05 +0500

    Made base routes on frontend

---
+++ client/src/routes.js
@@ -0,0 +1,36 @@
+import React from 'react'
+import {Switch, Route, Redirect} from 'react-router-dom'
+import { LinksPage } from './pages/LinksPage'
+import { CreatePage } from './pages/CreatePage'
+import { DetailPage } from './pages/DetailPage'
+import { AuthPage } from './pages/AuthPage'
+
+export const useRoutes = (isAuth) => {
+    if (isAuth) {
+        // TODO: Made 404 redirect if none of thoose roures has worked.
+        return (
+            <Switch>
+                <Route path="/links" exact>
+                    <LinksPage />
+                </Route>
+                <Route path="/create" exact>
+                    <CreatePage />
+                </Route>
+                <Route path="/detail/:id" exact>
+                    <DetailPage />
+                </Route>
+                
+            </Switch>
+        )
+    }
+    
+    // TODO: Made 404 redirect if none of thoose roures has worked.
+    return (
+        <Switch>
+            <Route to="/" exact>
+                <AuthPage />
+            </Route>
+        </Switch>
+    )
+
+}
